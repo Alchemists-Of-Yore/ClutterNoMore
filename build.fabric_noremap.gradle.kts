19e0180a58fc009ddcb8d03a77a288e8bcd44c70
Cassian <cassian@godsted.com>
2026-01-09 15:38:40 -0500

    publish v1.1.8

--- build.fabric_noremap.gradle.kts
+++ build.fabric_noremap.gradle.kts
@@ -165,7 +165,7 @@ publishMods {
     type = STABLE
     displayName = "${property("mod.name")} ${property("mod.version")} for ${stonecutter.current.version} Fabric"
     version = "${property("mod.version")}+${property("deps.minecraft")}-fabric"
-    changelog = provider { rootProject.file("CHANGELOG-LATEST.md").readText() }
+    changelog = provider { rootProject.file("CHANGELOG.md").readText() }
     modLoaders.add("fabric")
 
     modrinth {
@@ -179,7 +179,7 @@ publishMods {
     curseforge {
         projectId = property("publish.curseforge") as String
         accessToken = env.CURSEFORGE_API_KEY.orNull()
-        minecraftVersions.add(stonecutter.current.version)
+        minecraftVersions.add(property("publish.curseforge_minecraft_version").toString())
         minecraftVersions.addAll(additionalVersions)
         requires("fabric-api")
     }
